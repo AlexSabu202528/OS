39f35090d7c0710f7e4311cf0f1cfb66fffa23d6
Alexsabu <alexsabu053@gmail.com>
2026-03-18 13:39:26 +0530

    Add files via upload

---
+++ producer_consumer.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+sem_t resource;
+sem_t rmutex;     
+sem_t wmutex;     
+sem_t readTry;    
+
+int readcount = 0;
+int writecount = 0;
+int data = 0;
+
+void *reader(void *arg)
+{
+    int id = *(int *)arg;
+
+    sem_wait(&readTry);
+    sem_wait(&rmutex);
+
+    readcount++;
+    if (readcount == 1)
+        sem_wait(&resource);
+
+    sem_post(&rmutex);
+    sem_post(&readTry);
+
+    printf("Reader %d is reading data = %d\n", id, data);
+    sleep(1);
+
+    sem_wait(&rmutex);
+    readcount--;
+
+    if (readcount == 0)
+        sem_post(&resource);
+
+    sem_post(&rmutex);
+
+    return NULL;
+}
+
+void *writer(void *arg)
+{
+    int id = *(int *)arg;
+
+    sem_wait(&wmutex);
+    writecount++;
+    if (writecount == 1)
+        sem_wait(&readTry);
+    sem_post(&wmutex);
+
+    sem_wait(&resource);
+
+    data++;
+    printf("Writer %d is writing data = %d\n", id, data);
+    sleep(1);
+
+    sem_post(&resource);
+
+    sem_wait(&wmutex);
+    writecount--;
+    if (writecount == 0)
+        sem_post(&readTry);
+    sem_post(&wmutex);
+
+    return NULL;
+}
+
+int main()
+{
+    pthread_t r[5], w[3];
+    int id[5], wid[3];
+
+    sem_init(&resource, 0, 1);
+    sem_init(&rmutex, 0, 1);
+    sem_init(&wmutex, 0, 1);
+    sem_init(&readTry, 0, 1);
+
+    for (int i = 0; i < 5; i++)
+    {
+        id[i] = i + 1;
+        pthread_create(&r[i], NULL, reader, &id[i]);
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        wid[i] = i + 1;
+        pthread_create(&w[i], NULL, writer, &wid[i]);
+    }
+
+    for (int i = 0; i < 5; i++)
+        pthread_join(r[i], NULL);
+
+    for (int i = 0; i < 3; i++)
+        pthread_join(w[i], NULL);
+
+    return 0;
+}
